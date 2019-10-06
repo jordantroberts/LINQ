85214dea283573e67fa35c881603349d4fc18af9
Jordan Roberts <jordanroberts@talk21.com>
2019-10-06 17:34:25 +0100

    Displaying cheap books only

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using LINQ;
+using System.Collections.Generic;
 
 namespace LINQ
 {
@@ -6,6 +8,20 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            var books = new Library().GetBooks();
+
+            var cheapBooks = new List<Book>();
+            {
+                foreach (var book in books)
+            {
+                    if (book.Price < 10)
+                        cheapBooks.Add(book);
+            }
+
+                foreach (var book in cheapBooks)
+                    Console.WriteLine(book.Title + " £" + book.Price);
+
+            }
         }
     }
 }
